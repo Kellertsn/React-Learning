0349fd506e470e033d00bc8a77febf5b882f3f9a
Kellertsn <keller.tsn@gmail.com>
2025-07-28 10:51:19 -0700

    5th commit

--- react-training/src/components/Todolist/Todolist.jsx
+++ react-training/src/components/Todolist/Todolist.jsx
@@ -11,7 +11,12 @@ const Todolist = () => {
     setInput(e.target.value);
   };
   const handleAdd = () => {
-    const newTodo = { id: uuidv4(), title: input, isEditing: false, editText: "" };
+    const newTodo = {
+      id: uuidv4(),
+      title: input,
+      isEditing: false,
+      editText: "",
+    };
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
     setInput("");
@@ -21,17 +26,38 @@ const Todolist = () => {
     setTodos(todos.filter((item) => item.id !== inputId));
   };
 
-  const handleEdit = (inputId, isEdit = false) => {
+  const handleEditClick = (inputId) => {
     setTodos(
-      todos.map((item) => {
-        if (isEdit) {
-          if (item.id === input.id) {
-            return <li>S</li>;
-          }
-        }
-      })
+      todos.map((item) =>
+        item.id === inputId ? { ...item, isEditing: true, editText: item.title } : item
+      )
     );
   };
+
+  const handleEditChange = (inputId, newText) => {
+    setTodos(
+      todos.map((item) =>
+        item.id === inputId ? { ...item, editText: newText } : item
+      )
+    );
+  };
+
+  const handleSave = (inputId) => {
+    setTodos(todos.map(item =>
+      item.id === inputId
+      ? {...item, title: item.editText, isEditing: false, editText: ""}
+      : item
+    ))
+  }
+
+  const handleCancel = (inputId) => {
+    setTodos(todos.map(item =>
+      item.id === inputId
+      ? {...item, isEditing: false, editText: ""}
+      : item
+    ))
+  }
+
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <div>
@@ -42,8 +68,22 @@ const Todolist = () => {
         <ul>
           {todos.map((item) => (
             <li key={item.id}>
-              <span>{item.title}</span>
-              <button onClick={() => handleEdit(item.id)}>Edit</button>
+              {/* <span>{item.title}</span>
+              <button onClick={() => handleEdit(item.id)}>Edit</button> */}
+              {item.isEditing ? (
+                  <>
+                    <input value={item.editText} onChange={(e) => handleEditChange(item.id, e.target.value)} />
+                    <button onClick={() => handleSave(item.id)}>Save</button>
+                    <button onClick={() => handleCancel(item.id)}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    <span>{item.title}</span>
+                    <button onClick={() => handleEditClick(item.id)}>Edit</button>
+                  </>
+                )
+              }
+
               <button onClick={() => handleDelete(item.id)}>Delete</button>
             </li>
           ))}
