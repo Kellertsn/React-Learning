3cb24743924371970b3f03698ceb7420fc4b6d5a
Kellertsn <keller.tsn@gmail.com>
2025-08-01 18:22:10 -0700

    7th commit

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Todolist from './components/Todolist/Todolist.jsx'
-import CarApp from './components/ClassComponents/CarApp/CarApp.jsx'
-import CourseCatalog from './components/ClassComponents/7.30 HW/CourseCatalog.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Todolist from "./components/Todolist/Todolist.jsx";
+import CarApp from "./components/ClassComponents/CarApp/CarApp.jsx";
+import CourseCatalog from "./components/ClassComponents/7.30 HW/CourseCatalog.jsx";
+import ClassDemo from "./components/ClassComponents/ClassDemo.jsx";
+import HookDemo from "./components/HookDemo.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+  // const [show, setShow] = useState(true);
+  
   return (
     <>
-      {/* <Todolist /> */}
+      <Todolist />
       {/* <CarApp /> */}
-      <CourseCatalog />
+      {/* <CourseCatalog /> */}
+      {/* <button id="toggle-btn" onClick={() => setShow(!show)}>
+        Toggle show
+      </button>
+      {show && <HookDemo />} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
